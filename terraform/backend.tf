a5bf215e08893ddf344b6cacde71b8f2e9acf95e
Aman Gandhi <aggand@amazon.com>
2022-08-16 16:58:26 -0500

    fix: backend.tf file

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,11 +7,8 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    encrypt        = "true"
     bucket         = "573626210569-tf-remote-state"
-    dynamodb_table = "tf-state-lock"
     key            = "git://github.com/amankaurgandhi/codecov-demo.git"
     region         = "us-east-1"
-    profile        = "573626210569"
   }
 }
