3f278ff8792ea7205715f5feb610e0cf04a831fa
amankaurgandhi <amankaurgandhi@users.noreply.github.com>
2022-08-16 22:01:48 +0000

    Add terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,8 +8,11 @@ provider "aws" {
 
 terraform {
   backend "s3" {
+    encrypt        = "true"
     bucket         = "573626210569-tf-remote-state"
+    dynamodb_table = "tf-state-lock"
     key            = "git://github.com/amankaurgandhi/codecov-demo.git"
     region         = "us-east-1"
+    profile        = "573626210569"
   }
 }
