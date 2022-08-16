984805fd814353d95f96ea25139ac025b238872c
Aman Gandhi <aggand@amazon.com>
2022-08-16 16:46:28 -0500

    fix: backend tf

---
+++ .github/workflows/terraform/backend.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+
+  ignore_tags {
+    key_prefixes = ["AutoTag_"]
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "573626210569-tf-remote-state"
+    dynamodb_table = "tf-state-lock"
+    key            = "git://github.com/amankaurgandhi/codecov-demo.git"
+    region         = "us-east-1"
+    profile        = "573626210569"
+  }
+}
