0d0105d1eb88b30f2b9e62a7bcd688bd2473556e
Aman Gandhi <aggand@amazon.com>
2022-09-07 15:10:33 -0500

    test ouput of a run command

--- api/test.sh
+++ api/test.sh
@@ -3,8 +3,7 @@ RUNWHILE="True"
 while [ $RUNWHILE == "True" ]
 do
 RESULT="$(aws stepfunctions describe-execution --execution-arn "arn:aws:states:us-east-1:573626210569:execution:TestGitHUbActions:ed24be44-df06-4ce6-8fe1-b2d09ab8960d" --query 'status' --output text)"
-echo "::set-output name=finalresult::$RESULT"
-RESULT="RUNNING"
+echo "::set-output name=currentresult::$RESULT"
 if [[ $RESULT == "RUNNING" ]]
 then
 sleep 120
@@ -12,3 +11,4 @@ else
 RUNWHILE="False"
 fi
 done
+echo "::set-output name=finalresult::$RESULT"
