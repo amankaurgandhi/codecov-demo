1402cf02a89d9be8c79ba19fadfb633f94e913e4
Aman Gandhi <aggand@amazon.com>
2022-09-27 14:21:23 -0500

    test :

---
+++ terraform/backend.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+
+  ignore_tags {
+    key_prefixes = ["AutoTag_"]
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "573626210569-tf-remote-state"
+    key            = "git://github.com/amankaurgandhi/codecov-demo.git"
+    region         = "us-east-1"
+  }
+}
